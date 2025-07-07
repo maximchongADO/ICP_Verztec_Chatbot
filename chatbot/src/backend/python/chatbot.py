ed52205006058fffb3aebe302003ca0eae58b3b9
maximchongADO <maximchongzhss1e2@gmail.com>
2025-07-07 10:42:55 +0800

    Fix TTS

--- chatbot/src/backend/python/chatbot.py
+++ chatbot/src/backend/python/chatbot.py
@@ -40,7 +40,7 @@ logger = logging.getLogger(__name__)
 # Initialize models and clients
 embedding_model = SentenceTransformer('BAAI/bge-large-en-v1.5')
 load_dotenv()
-
+api_key='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'
 model = "deepseek-r1-distill-llama-70b" 
 deepseek = ChatGroq(api_key=api_key, model=model) # type: ignore
 
